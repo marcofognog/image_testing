ef91cba06c95310bd3515fe864149bf75f5e5561
Marco Antonio <marcofognog@gmail.com>
2013-07-06 14:11:25 -0300

    Lets take a step back and try with unidimensional cases.

--- spec/image_testing_spec.rb
+++ spec/image_testing_spec.rb
@@ -5,20 +5,22 @@ require 'image_testing'
 
 describe "image_testing" do
 
-  it "should find a segment of the image within the whole image" do
-    is_it_contained_in?("tomato2x2.gif", "tomato3x3.gif").should be(true)
-  end
-  
-  it "should NOT find the smaller image in the bigger image, because their different" do
-    is_it_contained_in?("tomato2x2.gif", "different_tomato.gif").should be(false)
-  end
+  context "Unidimensional" do
+    it "should find a segment of the image within the whole image" do
+      is_it_contained_in?("unidimensional/uni-4.gif", "unidimensional/uni-16.gif").should be(true)
+    end
 
-  it "should NOT find the smaller image in the bigger, because they'r not the same, but uses the same colors" do
-    is_it_contained_in?("tomato2x2.gif", "diff_with_same_colors_tomato.gif").should be(false)
-  end
-  
-  it "should find the smaller image in within the bigger, even if it is in other position" do
-    is_it_contained_in?("tomato2x2.gif", "2x2contained_in_other_position.gif").should be(true)
+    it "should NOT find the smaller image in the bigger image, because their different" do
+      is_it_contained_in?("unidimensional/diff-uni-4.gif", "unidimensional/uni-16.gif").should be(false)
+    end
+
+    it "should NOT find the smaller image in the bigger, because they'r not the same, but uses the same colors" do
+      is_it_contained_in?("unidimensional/uni-4.gif", "unidimensional/same-colors-uni-16.gif").should be(false)
+    end
+
+    it "should find the smaller image in within the bigger, even if it is in other position" do
+      is_it_contained_in?("unidimensional/uni-4.gif", "unidimensional/diff-position-uni-16.gif").should be(true)
+    end
   end
 
 end
