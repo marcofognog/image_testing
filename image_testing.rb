dd8c87fe8407966791d2e97c4153057d9a7b0b9f
Marco Antonio <marcofognog@gmail.com>
2013-07-06 16:01:51 -0300

    Consider the neighbor pixel. Passing all tests.

--- image_testing.rb
+++ image_testing.rb
@@ -2,10 +2,23 @@ require 'RMagick'
 
 include Magick
 
+class NoPixel
+  def color_equal?(pixel)
+    true
+  end
+end
+
 class Pixel
 
-  def initialize(rmagick_pixel)
+  def initialize(rmagick_pixel, row, col, image)
     @rmagick_pixel = rmagick_pixel
+    @row = row
+    @col = col
+    @image = image
+  end
+
+  def color_equal?(pixel)
+    color == pixel.color
   end
 
   def color
@@ -13,7 +26,13 @@ class Pixel
   end
 
   def right_neighbor
-    0
+    view = Image::View.new(@image, 0,0, @image.columns, @image.rows )
+    next_col = @col + 1
+    if next_col < @image.columns
+      Pixel.new(view[0][next_col], 0, next_col, @image)
+    else
+      NoPixel.new
+    end
   end
 
 end
@@ -21,12 +40,11 @@ end
 def is_it_contained_in?(contained_path, container_path)
   container = Image.read(container_path).first
   contained = Image.read(contained_path).first
-
   found_ar = []
 
-  contained.each_pixel do |ced_pixel|
-    container.each_pixel do |cer_pixel|
-      if pixels_match?(Pixel.new(ced_pixel), Pixel.new(cer_pixel))
+  contained.each_pixel do |ced_pixel, ced_c, ced_r|
+    container.each_pixel do |cer_pixel, cer_c, cer_r|
+      if pixels_match?(Pixel.new(ced_pixel, ced_r, ced_c, contained), Pixel.new(cer_pixel, cer_r , cer_c, container))
         found_ar << true
       end
     end
@@ -41,6 +59,8 @@ end
 
 
 def pixels_match?(ced_pixel, cer_pixel)
-  ced_pixel.color == cer_pixel.color && ced_pixel.right_neighbor == cer_pixel.right_neighbor
+  if ced_pixel.color == cer_pixel.color
+    ced_pixel.right_neighbor.color_equal?(cer_pixel.right_neighbor)
+  end
 end
 
