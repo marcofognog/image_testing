40a7a79fcb283bd4acfc8deae5c2006167450a39
Marco Antonio <marcofognog@gmail.com>
2013-07-06 12:41:13 -0300

    Faild spec trying to find the smal image in a different position.

--- spec/image_testing_spec.rb
+++ spec/image_testing_spec.rb
@@ -16,5 +16,9 @@ describe "image_testing" do
   it "should NOT find the smaller image in the bigger, because they'r not the same, but uses the same colors" do
     is_it_contained_in?("tomato2x2.gif", "diff_with_same_colors_tomato.gif").should be(false)
   end
+  
+  it "should find the smaller image in within the bigger, even if it is in other position" do
+    is_it_contained_in?("tomato2x2.gif", "2x2contained_in_other_position.gif").should be(true)
+  end
 
 end
