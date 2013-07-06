ec9b57f97dc377bf53600948c2382c244deecf8b
Marco Antonio <marcofognog@gmail.com>
2013-07-06 14:32:04 -0300

    Little refactoring: added Pixel class.

--- image_testing.rb
+++ image_testing.rb
@@ -2,6 +2,22 @@ require 'RMagick'
 
 include Magick
 
+class Pixel
+
+  def initialize(rmagick_pixel)
+    @rmagick_pixel = rmagick_pixel
+  end
+
+  def color
+    @rmagick_pixel.to_color(AllCompliance, false, QuantumDepth, true)
+  end
+
+  def right_neighbor
+    0
+  end
+
+end
+
 def is_it_contained_in?(contained_path, container_path)
   container = Image.read(container_path).first
   contained = Image.read(contained_path).first
@@ -10,7 +26,7 @@ def is_it_contained_in?(contained_path, container_path)
 
   contained.each_pixel do |ced_pixel|
     container.each_pixel do |cer_pixel|
-      if ced_pixel == cer_pixel
+      if pixels_match?(Pixel.new(ced_pixel), Pixel.new(cer_pixel))
         found_ar << true
       end
     end
@@ -22,3 +38,9 @@ def is_it_contained_in?(contained_path, container_path)
     return false
   end
 end
+
+
+def pixels_match?(ced_pixel, cer_pixel)
+  ced_pixel.color == cer_pixel.color && ced_pixel.right_neighbor == cer_pixel.right_neighbor
+end
+
